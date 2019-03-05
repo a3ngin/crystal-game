89be96a6dae1b51ce51dc26b1b002aa49d0c21a2
Anne Engin <anne@Annes-MacBook-Air.local>
2019-03-05 15:56:10 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,31 +17,64 @@ $(document).ready(function () {
     $("#losses-text").text(losses);
     $("#score-text").text(userscore);
 
-    $("#pink")
-    $("#green")
-    $("#blue")
-    $("#yellow")
+    // $("#pink")
+    // $("#green")
+    // $("#blue")
+    // $("#yellow")
 
     var crystalOne = Math.floor(Math.random() * 12 + 1);
     var crystalTwo = Math.floor(Math.random() * 12 + 1);
     var crystalThree = Math.floor(Math.random() * 12 +1);
     var crystalFour = Math.floor(Math.random() * 12 +1);
 
+    function reset() {
+        var randnum = Math.floor(Math.random() * 109) + 12;
+        $("#random-number").text(randnum);
+        userscore = 0;
+        $("#score-text").text(userscore);
+        var crystalOne = Math.floor(Math.random() * 12 + 1);
+        var crystalTwo = Math.floor(Math.random() * 12 + 1);
+        var crystalThree = Math.floor(Math.random() * 12 +1);
+        var crystalFour = Math.floor(Math.random() * 12 +1);
+   
+   }
+
+
     $("#blue").on("click", function() {
     console.log(crystalOne);
-    
+    userscore = crystalOne + userscore;
+    $("#score-text").text(userscore);
     });
 
     $("#green").on("click", function() {
     console.log(crystalTwo);
+    userscore = crystalTwo + userscore;
+    $("#score-text").text(userscore);
     });
 
     $("#pink").on("click", function() {
     console.log(crystalThree);
+    userscore = crystalThree + userscore;
+    $("#score-text").text(userscore);
     });
 
     $("#yellow").on("click", function() {
     console.log(crystalFour);
+    userscore = crystalFour + userscore;
+    $("#score-text").text(userscore);
+
+    if(userscore === randnum) {
+        wins++;
+        $("#wins-text").text(wins);
+        reset();
+    }
+    else if(userscore > randnum) {
+        losses++;
+        $("#losses-text").text(losses);
+        reset();
+    }
     });
+
+     
 
 });
